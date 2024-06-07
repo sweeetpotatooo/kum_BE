00034149d6768e28ee176d4e0666ad9798550e9b
sweeetpotaooo <sehyun5004@naver.com>
2024-06-07 14:47:17 +0900

    userdetail

--- src/models/users.js
+++ src/models/users.js
@@ -18,7 +18,51 @@ const User = sequelize.define('User', {
     password: {
         type: DataTypes.STRING,
         allowNull: false
-    }
+    },
+
+    nickname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    studentid: {
+        type: DataTypes.INT,
+        allowNull: false
+    },
+
+    department: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+
+    age: {
+        type: DataTypes.INT,
+        allowNull: false
+    },
+
+
+    smoke: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+
+    
+    snoring: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+
+    bruxism: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+
+
+    detail: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    },
 }, {
     // Other model options go here
 });
